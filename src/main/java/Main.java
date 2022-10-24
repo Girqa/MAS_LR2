282fff359c22774252dfa256522a0e3b7f634353
Yarik <kalter0901@gmail.com>
2022-10-24 10:48:31 +0300

    Realized launching agents via annotation.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,47 @@
-import AgentsFactory.AgentFactory;
-import AgentsFactory.AgentDescription;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import Agents.FunctionAgent;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Set;
+
+import Annotations.AutoRunnableAgent;
+import jade.core.ProfileImpl;
+import jade.core.Runtime;
+import jade.util.leap.Properties;
+import org.reflections.Reflections;
+
 
 public class Main {
     public static void main(String[] args) {
-        List<AgentDescription> agentsDescs = Arrays.asList(
-                new AgentDescription("A1", FunctionAgent.class),
-                new AgentDescription("A2", FunctionAgent.class),
-                new AgentDescription("A3", FunctionAgent.class)
-        );
-        AgentFactory.createAgents(agentsDescs);
+        Reflections r = new Reflections("Agents");
+
+        Map<String, String> agents =new HashMap<>();
+
+        Set<Class<?>> classes = r.getTypesAnnotatedWith(AutoRunnableAgent.class);
+        for (Class<?> annotatedClass: classes) {
+            AutoRunnableAgent anno = annotatedClass.getAnnotation(AutoRunnableAgent.class);
+            System.out.println(annotatedClass.getPackageName());
+            for (int i = 0; i < anno.count(); i++) {
+                agents.put(anno.name()+(i+1), "Agents.FunctionAgent");
+            }
+        }
+
+        Properties pp = parseCmdLineArgs(agents);
+        ProfileImpl p = new ProfileImpl(pp);
+        Runtime.instance().setCloseVM(true);
+        Runtime.instance().createMainContainer(p);
+
+
+    }
+
+    private static Properties parseCmdLineArgs(Map<String, String> createdAgents) {
+        Properties props = new Properties();
+        props.setProperty("gui", "true");
+
+        StringBuilder agents = new StringBuilder();
+
+        for (Map.Entry<String, String> entry: createdAgents.entrySet()) {
+            agents.append(entry.getKey()).append(":").append(entry.getValue()).append(";");
+        }
+        props.setProperty("agents", agents.toString());
+        return props;
     }
 }
